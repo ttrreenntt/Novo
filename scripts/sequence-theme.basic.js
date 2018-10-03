3f7a279955d6e9ebe0115d5c92e79a251be20912
Joshua Marino <joshuamarino@gmail.com>
2018-10-03 17:22:47 +0000

    Upload: scripts/sequence-theme.basic.js

--- scripts/sequence-theme.basic.js
+++ scripts/sequence-theme.basic.js
@@ -24,14 +24,13 @@ var sequenceElement = document.getElementById("sequence");
 // Place your Sequence options here to override defaults
 // See: http://sequencejs.com/documentation/#options
 var options = {
-	keyNavigation: true,
   startingStepAnimatesIn: true,
   autoPlay: true,
   autoPlayInterval: 2000,
   phaseThreshold: 250,
   preloader: true,
   reverseWhenNavigatingBackwards: true,
-  fadeStepWhenSkipped: false;
+  fadeStepWhenSkipped: false
 }
 
 // Launch Sequence on the element, and with the options we specified above
