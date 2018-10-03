df08ecbd51cff8fe3489a84b9003ebba6f1a9ed2
Joshua Marino <joshuamarino@gmail.com>
2018-10-03 21:16:49 +0000

    Upload: scripts/sequence-theme.basic.js

--- scripts/sequence-theme.basic.js
+++ scripts/sequence-theme.basic.js
@@ -24,7 +24,7 @@ var sequenceElement = document.getElementById("sequence");
 // Place your Sequence options here to override defaults
 // See: http://sequencejs.com/documentation/#options
 var options = {
-	autoPlay: true,
+	autoPlay: false,
 	startingStepAnimatesIn: true,
 	phaseThreshold: 250,
 	preloader: true,
