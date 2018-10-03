f8ea526ebb79735cbf8890bf1679d58c8d38862c
Joshua Marino <joshuamarino@gmail.com>
2018-10-03 17:20:33 +0000

    Upload: scripts/custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,8 +1,23 @@
 // Functions
 
 // On Page Load
-$(document).ready(function () {
-
-	 
+$( document ).ready(function() {
+	// Get the Sequence element
+	var sequenceElement = document.getElementById("sequence");
 	
-});
+	// Place your Sequence options here to override defaults
+	// See: http://sequencejs.com/documentation/#options
+	var options = {
+		keyNavigation: true,
+	  startingStepAnimatesIn: true,
+	  autoPlay: true,
+	  autoPlayInterval: 2000,
+	  phaseThreshold: 250,
+	  preloader: true,
+	  reverseWhenNavigatingBackwards: true,
+	  fadeStepWhenSkipped: false
+	}
+	
+	// Launch Sequence on the element, and with the options we specified above
+	var mySequence = sequence(sequenceElement, options);
+}); 
