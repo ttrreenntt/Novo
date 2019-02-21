1455409267da3aaa81a975db3446524c1091d4dd
Joshua Marino <joshuamarino@gmail.com>
2019-02-21 02:00:59 +0000

    Upload: scripts/site-bundle.js

--- scripts/site-bundle.js
+++ scripts/site-bundle.js
@@ -91,56 +91,76 @@ var tilt = __webpack_require__(8);
 
 // Action Animations
 function hideControls(player) {
-  $(player).addClass('hide-controls');
+				$(player).addClass('hide-controls');
 }
 
 function toggleFeatured() {
-  $('aside').toggleClass('show-featured');
+				$('aside').toggleClass('show-featured');
 }
 
 function toggleNav() {
-  $('body').toggleClass('show-nav');
+				$('body').toggleClass('show-nav');
 }
 
 function toggleBtn(btn) {
-  $(btn).toggleClass('close-btn');
-  $(btn).toggleClass('icon-arrow-right');
-  $(btn).toggleClass('icon-close');
+				$(btn).toggleClass('close-btn');
+				$(btn).toggleClass('icon-arrow-right');
+				$(btn).toggleClass('icon-close');
 }
 
 function addArrows() {
-  $('.form-button-wrapper').addClass('icon-arrow-right');
+				$('.form-button-wrapper').addClass('icon-arrow-right');
+}
+
+// Pick Blog Post
+function pickBlog() {
+				var blog = Math.ceil(Math.random() * 3);
+
+				switch (blog) {
+								case 1:
+												// Keep the Discipleship Blog visible
+												break;
+								case 2:
+												$('.discipleship').toggleClass('show');
+												$('.mission').toggleClass('show');
+												// Switch to Mission Blog
+												break;
+								case 3:
+												$('.discipleship').toggleClass('show');
+												$('.action').toggleClass('show');
+								// Switch to Action Blog 
+				}
 }
 
 $(document).ready(function () {
 
-  // Hide Video Controls
-  $('.sqs-video-wrapper').click(function () {
-    hideControls(this);
-  });
+				// Hide Video Controls
+				$('.sqs-video-wrapper').click(function () {
+								hideControls(this);
+				});
 
-  // Show / Hide Featured Content
-  $('.featured-button').click(function () {
-    toggleFeatured();
-  });
+				// Show / Hide Featured Content
+				$('.featured-button').click(function () {
+								toggleFeatured();
+				});
 
-  // Show / Hide Mobile Nae
-  $('.open-nav').click(function () {
-    toggleNav();
-  });
-  $('.close-nav').click(function () {
-    toggleNav();
-  });
+				// Show / Hide Mobile Nae
+				$('.open-nav').click(function () {
+								toggleNav();
+				});
+				$('.close-nav').click(function () {
+								toggleNav();
+				});
 
-  // Replace Button Text
-  $('.featured-button').click(function () {
-    toggleBtn(this);
-  });
+				// Replace Button Text
+				$('.featured-button').click(function () {
+								toggleBtn(this);
+				});
 
-  // Add Arrows to Form Buttons
-  $('.sqs-editable-button').click(function () {
-    addArrows();
-  });
+				// Add Arrows to Form Buttons 
+				$('.sqs-editable-button').click(function () {
+								addArrows();
+				});
 });
 
 // Loading Animations
@@ -149,26 +169,29 @@ document.body.classList.add('js-loading');
 window.addEventListener("load", showPage);
 
 function showPage() {
-  document.body.classList.remove('js-loading');
+				document.body.classList.remove('js-loading');
 
-  $('header').addClass('show');
+				$('header').addClass('show');
 
-  // Fix Footer Buttons
-  $('span.sqs-editable-button').addClass('icon-arrow-right');
-  $('.subscribe-content .sqs-editable-button').html("<strong>Sign Up</strong>");
-  $('.contact-content .sqs-editable-button').html("Send an <strong>Email</strong>");
+				// Fix Footer Buttons
+				$('span.sqs-editable-button').addClass('icon-arrow-right');
+				$('.subscribe-content .sqs-editable-button').html("<strong>Sign Up</strong>");
+				$('.contact-content .sqs-editable-button').html("Send an <strong>Email</strong>");
+
+				// Pick Blog Post
+				pickBlog();
 }
 
 // SQSP Functions
 window.addEventListener('DOMContentLoaded', function () {
 
-  var images = document.querySelectorAll('img[data-src]');
+				var images = document.querySelectorAll('img[data-src]');
 
-  for (var i = 0; i < images.length; i++) {
-    core.ImageLoader.load(images[i], {
-      load: true
-    });
-  }
+				for (var i = 0; i < images.length; i++) {
+								core.ImageLoader.load(images[i], {
+												load: true
+								});
+				}
 });
 
 /***/ }),
