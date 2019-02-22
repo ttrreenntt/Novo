a3e190d8852c7673bf2b274ba43e0e1da3420499
Joshua Marino <joshuamarino@gmail.com>
2019-02-22 18:13:20 +0000

    Upload: scripts/default.js

--- scripts/default.js
+++ scripts/default.js
@@ -1,102 +1,37 @@
+/**
+ * A basic JavaScript file that refreshes the Squarespace ImageLoader. For more
+ * information about writing custom JavaScript on a Squarespace developer site
+ * visit the link below.
+ * @see http://developers.squarespace.com/custom-javascript/
+ *
+ * This script wrapped in a Immediately-Invoked Function Expression (IIFE) to
+ * prevent variables from leaking onto the global scope. For more information
+ * on IIFE visit the link below.
+ * @see http://en.wikipedia.org/wiki/Immediately-invoked_function_expression
+ */
+(function() {
+  'use strict';
 
-// Action Animations 
-function hideControls(player) {
-    $(player).addClass('hide-controls');
-}
+  // Stop the script if the user is on an old browser.
+  // Browser support: http://caniuse.com/#search=queryselectorall
+  if (!document.querySelectorAll) {
+    return;
+  }
 
-function toggleFeatured() {
-    $('aside').toggleClass('show-featured');
-}
+  /**
+   * Loads all images on the page using Squarespace's Responsive ImageLoader.
+   *
+   * @method loadImages
+   * @see http://developers.squarespace.com/using-the-imageloader/
+   */
+  function loadAllImages() {
+    var images = document.querySelectorAll('img[data-src]' );
 
-function toggleNav() {
-    $('body').toggleClass('show-nav');
-}
-
-function toggleBtn(btn) {
-	$(btn).toggleClass('close-btn');
-	$(btn).toggleClass('icon-arrow-right');
-	$(btn).toggleClass('icon-close');
-} 
- 
-function addArrows() {
-    $('.form-button-wrapper').addClass('icon-arrow-right');
-}
-
-
-// Pick Blog Post
-function pickBlog() {
-    const blog = Math.ceil(Math.random() * 3);
-    
-    switch(blog) {
-	    case 1:
-		// Keep the Discipleship Blog visible
-	    break;
-	    case 2:
-	    $('.discipleship').toggleClass('show');
-	    $('.mission').toggleClass('show');
-	    // Switch to Mission Blog
-	    break;
-	    case 3:
-	    $('.discipleship').toggleClass('show');
-	    $('.action').toggleClass('show'); 
-	    // Switch to Action Blog 
+    for (var i = 0; i < images.length; i++) {
+      ImageLoader.load(images[i]);
     }
-}
+  }
 
-
-$(document).ready(function () {
-	
-	// Hide Video Controls
-	$('.sqs-video-wrapper').click(function(){ hideControls(this); });
-	
-	// Show / Hide Featured Content
-	$('.featured-button').click(function(){ toggleFeatured(); });
-	
-	// Show / Hide Mobile Nae
-	$('.open-nav').click(function(){ toggleNav(); });
-	$('.close-nav').click(function(){ toggleNav(); });
-	
-	// Replace Button Text
-	$('.featured-button').click(function(){ toggleBtn(this); });
-	
-	// Add Arrows to Form Buttons 
-	$('.sqs-editable-button').click(function(){ addArrows(); });
-	
-});
-
-
-// Loading Animations
-document.body.classList.add('js-loading');
-
-window.addEventListener("load", showPage);
-
-function showPage() {
-  document.body.classList.remove('js-loading');
-  
-  $('header').addClass('show');
-  
-  // Fix Footer Buttons
-  $('span.sqs-editable-button').addClass('icon-arrow-right');
-  $('.subscribe-content .sqs-editable-button').html("<strong>Sign Up</strong>"); 
-  $('.contact-content .sqs-editable-button').html("Send an <strong>Email</strong>");
-  
-  // Pick Blog Post
-  pickBlog();
-  
-  
-}
-
-
-// SQSP Functions
-window.addEventListener('DOMContentLoaded', function() {
-
-  const images = document.querySelectorAll('img[data-src]');
-
-  for (let i = 0; i < images.length; i++) {
-    core.ImageLoader.load(images[i], {
-      load: true
-    });
-  } 
-
-});
-
+  // The event subscription that reloads images on resize.
+  document.addEventListener('resize', loadAllImages);
+}());
